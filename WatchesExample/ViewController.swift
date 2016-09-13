97384071a0cd109c4713351fa126cb4ca190b12a
Tai Vuong <vhuutai@gmail.com>
2016-09-14 01:01:32 +0700

    Add project

---
+++ WatchesExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WatchesExample
+//
+//  Created by HuuTaiVuong on 9/13/16.
+//  Copyright © 2016 Huu Tai Vuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
